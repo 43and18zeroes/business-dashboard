8470b52c69a3dbd6f67730f69ec933876b2c47a5
Christoph <c.wagner.germany@gmail.com>
2026-01-02 15:57:58 +0100

    chart

---
+++ src/app/components/charts/bar-chart/bar-chart.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { BaseChartComponent } from '../../base-chart-component/base-chart-component';
+import { ChartConfiguration, ChartData } from '../../../models/chart.model';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'app-bar-chart',
+  imports: [],
+  templateUrl: './bar-chart.html',
+  styleUrl: './bar-chart.scss',
+})
+export class BarChart extends BaseChartComponent {
+  
+  // Hier füllen wir die "unbekannte Zutat" aus dem Grundrezept
+  protected renderChart(data: ChartData[], config: ChartConfiguration): void {
+    // Falls schon ein Chart existiert (z.B. bei Daten-Update), löschen wir das alte
+    if (this.chartInstance) {
+      this.chartInstance.destroy();
+    }
+
+    // Wir greifen auf 'this.canvas' zu, das in der Basisklasse definiert wurde!
+    this.chartInstance = new Chart(this.canvas.nativeElement, {
+      type: 'bar', 
+      data: {
+        labels: data.map(d => d.label),
+        datasets: [{
+          label: config.title,
+          data: data.map(d => d.value),
+          backgroundColor: data.map(d => d.color || '#42A5F5')
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: { display: config.showLegend }
+        }
+      }
+    });
+  }
+}
--- src/app/pages/start/start.ts
+++ src/app/pages/start/start.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BarChart } from '../../components/charts/bar-chart/bar-chart';
+import { ChartConfiguration } from '../../models/chart.model';
+import { ChartDataService } from '../../services/chart-data-service';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [BarChart],
   templateUrl: './start.html',
-  styleUrl: './start.scss'
+  styleUrl: './start.scss',
 })
 export class Start {
+  protected chartService = inject(ChartDataService);
 
+  // Konfiguration über deine Klasse erstellen
+  myChartConfig = new ChartConfiguration({
+    title: 'Monthly Sales',
+    showLegend: true,
+  });
 }
