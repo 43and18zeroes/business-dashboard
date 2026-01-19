75bce9ae8b28236b2cdd0dfce40227c24c383e54
cw <c.wagner.germany@gmail.com>
2026-01-19 15:43:26 +0100

    dragable-table

---
+++ src/app/components/dragable-table/dragable-table.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DragableTable } from './dragable-table';
+
+describe('DragableTable', () => {
+  let component: DragableTable;
+  let fixture: ComponentFixture<DragableTable>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DragableTable]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DragableTable);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
