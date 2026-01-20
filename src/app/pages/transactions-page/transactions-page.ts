a61a598dfa4caaa6086dbcd7177d8ac49a5a4867
cw <c.wagner.germany@gmail.com>
2026-01-20 17:21:45 +0100

    transactionCellFormatter2

--- src/app/pages/transactions-page/transactions-page.ts
+++ src/app/pages/transactions-page/transactions-page.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { DragableTableComponent } from "../../components/dragable-table-component/dragable-table-component";
 import { MockDataService } from '../../services/mock-data-service';
+import { transactionCellFormatter } from '../../shared/table-formatters';
 
 @Component({
   selector: 'app-transactions-page',
@@ -10,4 +11,6 @@ import { MockDataService } from '../../services/mock-data-service';
 })
 export class TransactionsPage {
   protected chartService = inject(MockDataService);
+
+  transactionCellFormatter = transactionCellFormatter;
 }
