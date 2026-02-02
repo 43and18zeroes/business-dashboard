73beb91069aa22ca5cc4999f864258fa4c456b91
cw <c.wagner.germany@gmail.com>
2026-02-02 13:02:37 +0100

    new table

---
+++ src/app/components/dragable-table-component-2/dragable-table-component-2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragable-table-component-2',
+  imports: [],
+  templateUrl: './dragable-table-component-2.html',
+  styleUrl: './dragable-table-component-2.scss',
+})
+export class DragableTableComponent2 {
+
+}
--- src/app/pages/transactions-page/transactions-page.ts
+++ src/app/pages/transactions-page/transactions-page.ts
@@ -1,37 +1,38 @@
 import { Component, inject } from '@angular/core';
-import { DragableTableComponent } from "../../components/dragable-table-component/dragable-table-component";
-import { MockDataService } from '../../services/mock-data-service';
-import { transactionCellFormatter } from '../../shared/table-formatters';
+// import { DragableTableComponent } from "../../components/dragable-table-component/dragable-table-component";
+// import { MockDataService } from '../../services/mock-data-service';
+// import { transactionCellFormatter } from '../../shared/table-formatters';
+import { DragableTableComponent2 } from "../../components/dragable-table-component-2/dragable-table-component-2";
 
 @Component({
   selector: 'app-transactions-page',
-  imports: [DragableTableComponent],
+  imports: [DragableTableComponent2],
   templateUrl: './transactions-page.html',
   styleUrl: './transactions-page.scss',
 })
 export class TransactionsPage {
-  protected chartService = inject(MockDataService);
+  // protected chartService = inject(MockDataService);
 
-  transactionCellFormatter = transactionCellFormatter;
+  // transactionCellFormatter = transactionCellFormatter;
 
-  transactionColumns: string[] = [];
+  // transactionColumns: string[] = [];
 
-  private mediaQuery = window.matchMedia('(min-width: 600px)');
-  private mediaListener = (e: MediaQueryListEvent) =>
-    this.updateColumns(e.matches);
+  // private mediaQuery = window.matchMedia('(min-width: 600px)');
+  // private mediaListener = (e: MediaQueryListEvent) =>
+  //   this.updateColumns(e.matches);
 
-  constructor() {
-    this.updateColumns(this.mediaQuery.matches);
-    this.mediaQuery.addEventListener('change', this.mediaListener);
-  }
+  // constructor() {
+  //   this.updateColumns(this.mediaQuery.matches);
+  //   this.mediaQuery.addEventListener('change', this.mediaListener);
+  // }
 
-  ngOnDestroy() {
-    this.mediaQuery.removeEventListener('change', this.mediaListener);
-  }
+  // ngOnDestroy() {
+  //   this.mediaQuery.removeEventListener('change', this.mediaListener);
+  // }
 
-  private updateColumns(isWide: boolean) {
-    this.transactionColumns = isWide
-      ? ['txId', 'user', 'cost', 'date']
-      : ['txId', 'user'];
-  }
+  // private updateColumns(isWide: boolean) {
+  //   this.transactionColumns = isWide
+  //     ? ['txId', 'user', 'cost', 'date']
+  //     : ['txId', 'user'];
+  // }
 }
