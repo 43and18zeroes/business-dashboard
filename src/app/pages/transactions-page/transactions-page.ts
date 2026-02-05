585e4e8c3888f853d49fc71da448cbfda972f5d5
cw <c.wagner.germany@gmail.com>
2026-02-05 08:45:08 +0100

    transaction table

--- src/app/pages/transactions-page/transactions-page.ts
+++ src/app/pages/transactions-page/transactions-page.ts
@@ -2,15 +2,42 @@ import { Component, inject } from '@angular/core';
 // import { DragableTableComponent } from "../../components/dragable-table-component/dragable-table-component";
 // import { MockDataService } from '../../services/mock-data-service';
 // import { transactionCellFormatter } from '../../shared/table-formatters';
-import { DragableTableComponent2 } from "../../components/dragable-table-component-2/dragable-table-component-2";
+import { DragableTableComponent2, ReorderEvent } from "../../components/dragable-table-component-2/dragable-table-component-2";
+import { MockDataService } from '../../services/mock-data-service';
+import { TRANSACTIONS } from '../../services/mock-data.constant';
+
+type Transaction = (typeof TRANSACTIONS)[number];
 
 @Component({
   selector: 'app-transactions-page',
-  // imports: [DragableTableComponent2],
+  imports: [DragableTableComponent2],
   templateUrl: './transactions-page.html',
   styleUrl: './transactions-page.scss',
 })
 export class TransactionsPage {
+
+  protected chartService = inject(MockDataService);
+
+  transactionColumns = ['txId', 'user', 'date', 'cost'] as const;
+
+  onTransactionsReorder(ev: ReorderEvent<Transaction>) {
+    console.log('[REORDER] Persist this order in backend:', {
+      entity: 'transactions',
+      idKey: 'txId',
+      moved: { from: ev.previousIndex, to: ev.currentIndex },
+      orderedIds: ev.orderedIds,
+    });
+  }
+
+  cellFormatter = (col: string, value: unknown) => {
+    if (col === 'cost' && typeof value === 'number') {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(value);
+    }
+    return String(value ?? '');
+  };
   // protected chartService = inject(MockDataService);
 
   // transactionCellFormatter = transactionCellFormatter;
