2d311b775e46c5a957f611de12e2d3b20c49c54e
cw <c.wagner.germany@gmail.com>
2026-02-21 18:32:18 +0100

    calendar comp

--- src/app/components/calender-component/calender-component.ts
+++ src/app/components/calender-component/calender-component.ts
@@ -1,11 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  CalendarView,
+  CalendarEvent,
+
+  CalendarMonthViewComponent,
+  CalendarWeekViewComponent,
+  CalendarDayViewComponent,
+
+  CalendarTodayDirective,
+  CalendarNextViewDirective,
+  CalendarPreviousViewDirective,
+  CalendarDatePipe,
+} from 'angular-calendar';
 
 @Component({
   selector: 'app-calender-component',
-  imports: [],
+  imports: [
+    CommonModule,
+
+    CalendarTodayDirective,
+    CalendarNextViewDirective,
+    CalendarPreviousViewDirective,
+    CalendarDatePipe,
+
+    CalendarMonthViewComponent,
+    CalendarWeekViewComponent,
+    CalendarDayViewComponent,
+  ],
   templateUrl: './calender-component.html',
   styleUrl: './calender-component.scss',
 })
 export class CalenderComponent {
+  CalendarView = CalendarView;
 
+  view: CalendarView = CalendarView.Month;
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [
+    { start: new Date(), title: 'Test-Event' },
+  ];
+
+  addEvent(date: Date) {
+    this.events = [
+      ...this.events,
+      { title: 'Neues Event', start: date },
+    ];
+  }
 }
