b013287d8d999628e8fa28aaaa886f97525f4c7f
cw <c.wagner.germany@gmail.com>
2026-02-22 10:19:43 +0100

    calendar working

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Chart, registerables } from 'chart.js';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 Chart.register(...registerables);
 
