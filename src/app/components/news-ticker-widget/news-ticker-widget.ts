02f9d3620d8b61534a149aad5ecfaefab3e5f9b5
cw <c.wagner.germany@gmail.com>
2026-03-01 14:57:55 +0100

    api

--- src/app/components/news-ticker-widget/news-ticker-widget.ts
+++ src/app/components/news-ticker-widget/news-ticker-widget.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NewsTickerService } from './news-ticker-service';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-news-ticker-widget',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './news-ticker-widget.html',
   styleUrl: './news-ticker-widget.scss',
 })
 export class NewsTickerWidget {
+  private newsTickerService = inject(NewsTickerService);
 
+  news = this.newsTickerService.articles;
+
+  ngOnInit() {
+    this.newsTickerService.fetchFinanceNews();
+  }
 }
