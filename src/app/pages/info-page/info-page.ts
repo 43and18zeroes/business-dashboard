51ae8d6e1482a037f30cfb6b19fa4a6781d3ecdc
cw <c.wagner.germany@gmail.com>
2026-03-24 17:24:45 +0100

    info 2

--- src/app/pages/info-page/info-page.ts
+++ src/app/pages/info-page/info-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-info-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './info-page.html',
+    styleUrl: './info-page.scss',
 })
 export class InfoPage {
 
